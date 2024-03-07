585bb3df2f9a7426df2da8af7b7dc65a4e1688e6
GiovanniMarseglia <giovannimarseglia00@gmail.com>
2024-03-07 11:55:22 +0100

    update

--- index.php
+++ index.php
@@ -1,12 +1,3 @@
-<!-- Oggi pomeriggio ripassate i primi concetti di classe, variabili e metodi d'istanza che abbiamo visto stamattina e create un file index.php in cui:
- - è definita una classe ‘Movie’
-   => all'interno della classe sono dichiarate delle variabili d'istanza
-   => all'interno della classe è definito un costruttore
-   => all'interno della classe è definito almeno un metodo
-- vengono istanziati almeno due oggetti ‘Movie’ e stampati a schermo i valori delle relative proprietà -->
-
-
-
 <?php
 
     class Movie{
@@ -28,10 +19,10 @@
 
     $primo=new Movie(1,'LA ZONA D INTERESSE','23/03/22');
     $primo->stampamovie();
-    ?>
-    <?php
+    
+    
         $secondo=new Movie(2,'DRIVE-AWAY DOLLS','20/05/21');
         $secondo->stampamovie();
-        ?>
+        
     
     
